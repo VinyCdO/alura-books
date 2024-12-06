cd8944dc3a720aca82f67f94c8799981d44a331a
Vinicius <vinicius0793@gmail.com>
2024-12-06 00:52:18 -0300

    Implementando containers e styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
 import Header from './componentes/header';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(180deg, #fff 15%, #002F52 );
+`
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header />      
-    </div>
+    </AppContainer>
   );
 }
 
--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,12 +1,21 @@
-import './estilo.css';
 import Logo from '../../imagens/logo.svg';
+import styled from 'styled-components';
+
+const LogoContainer = styled.div`
+  display: flex;  
+  font-size: 30px;
+`
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+`
 
 function logo() {
   return (
-    <div className='logo'>
-      <img src={Logo} alt='logo' className='logo-img'></img>
+    <LogoContainer>
+      <LogoImage src={Logo} alt='logo' className='logo-img' />
       <p><strong>Alura</strong>Books</p>
-    </div>
+    </LogoContainer>
   )
 }
 
