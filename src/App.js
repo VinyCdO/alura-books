9808f6a775433f064f4e0d898453eb2697b218b0
Vinicius <vinicius0793@gmail.com>
2024-12-09 01:16:07 -0300

    Implementação de controle de estado para pesquisa

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Header from './componentes/header';
+import Pesquisa from './componentes/pesquisa';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: linear-gradient(180deg, #fff 15%, #002F52 );
+  background-image: linear-gradient(180deg, #fff 15%, #002F52 100%);
 `
 
 function App() {
   return (
     <AppContainer>
       <Header />      
+      <Pesquisa />
     </AppContainer>
   );
 }
